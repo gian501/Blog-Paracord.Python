c62799aae47c3a8e93b02e62aa68efd4076d0d2c
Gianfranco Valente Cano <gianny@MBPdeGianfranco.lan>
2023-03-04 22:48:47 -0300

    Checkpoint. Modificacion de usuarios mediante la creacion de perfiles.Crearemos el modelo y el CRUD

--- AppECP/models.py
+++ AppECP/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+	image = models.ImageField(default='users/image_user.png', upload_to='users/')
+	biografia = models.TextField(max_length=400, null=True, blank=True)
+
+	class Meta:
+		verbose_name = 'Perfil'
+		verbose_name_plural = 'Perfiles'
+		ordering = ['-id']
+
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
